d8932cb99baa29f40cb30db27cd2d9c34185af26
Ross Fairbanks <rossf7@users.noreply.github.com>
2020-10-07 13:37:26 +0200

    Remove unused AWS code (#3)
    * Remove unused AWS code
    
    * go mod tidy
    
    * Disable codeowners

--- pkg/unittest/default_admission_request.go
+++ pkg/unittest/default_admission_request.go
@@ -10,30 +10,6 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
-func DefaultAdmissionRequestAWSControlPlane() (v1beta1.AdmissionRequest, error) {
-	byt, err := json.Marshal(DefaultAWSControlPlane())
-	if err != nil {
-		return v1beta1.AdmissionRequest{}, microerror.Mask(err)
-	}
-
-	req := v1beta1.AdmissionRequest{
-		Kind: metav1.GroupVersionKind{
-			Version: "infrastructure.giantswarm.io/v1alpha2",
-			Kind:    "AWSControlPlane",
-		},
-		Resource: metav1.GroupVersionResource{
-			Version:  "infrastructure.giantswarm.io/v1alpha2",
-			Resource: "awscontrolplanes",
-		},
-		Operation: v1beta1.Create,
-		Object: runtime.RawExtension{
-			Raw:    byt,
-			Object: nil,
-		},
-	}
-	return req, nil
-}
-
 func DefaultAdmissionRequestG8sControlPlane() (v1beta1.AdmissionRequest, error) {
 	byt, err := json.Marshal(DefaultG8sControlPlane())
 	if err != nil {
@@ -48,30 +24,6 @@ func DefaultAdmissionRequestG8sControlPlane() (v1beta1.AdmissionRequest, error) 
 		Resource: metav1.GroupVersionResource{
 			Version:  "infrastructure.giantswarm.io/v1alpha2",
 			Resource: "g8scontrolplanes",
-		},
-		Operation: v1beta1.Create,
-		Object: runtime.RawExtension{
-			Raw:    byt,
-			Object: nil,
-		},
-	}
-	return req, nil
-}
-
-func DefaultAdmissionRequestAWSMachineDeployment() (v1beta1.AdmissionRequest, error) {
-	byt, err := json.Marshal(DefaultAWSMachineDeployment())
-	if err != nil {
-		return v1beta1.AdmissionRequest{}, microerror.Mask(err)
-	}
-
-	req := v1beta1.AdmissionRequest{
-		Kind: metav1.GroupVersionKind{
-			Version: "infrastructure.giantswarm.io/v1alpha2",
-			Kind:    "AWSMachineDeployment",
-		},
-		Resource: metav1.GroupVersionResource{
-			Version:  "infrastructure.giantswarm.io/v1alpha2",
-			Resource: "awsmachinedeployments",
 		},
 		Operation: v1beta1.Create,
 		Object: runtime.RawExtension{

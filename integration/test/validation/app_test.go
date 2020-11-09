afc1ad0a738c26cf7264f3dea7fd5338b60b945b
Ross Fairbanks <ross@rossfairbanks.com>
2020-11-09 10:53:19 +0100

    Add integration test for skip on delete logic

--- integration/test/validation/app_test.go
+++ integration/test/validation/app_test.go
@@ -12,9 +12,13 @@ import (
 	"github.com/giantswarm/apiextensions/v3/pkg/label"
 	"github.com/giantswarm/backoff"
 	"github.com/giantswarm/microerror"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// TestFailWhenCatalogNotFound tests that the app CR is rejected if the
+// referenced appcatalog CR does not exist.
 func TestFailWhenCatalogNotFound(t *testing.T) {
 	ctx := context.Background()
 
@@ -62,4 +66,97 @@ func TestFailWhenCatalogNotFound(t *testing.T) {
 	}
 
 	logger.LogCtx(ctx, "level", "debug", "message", "waited for failed app creation")
+}
+
+// TestSkipValidationOnDelete tests that the validation is skipped when the app
+// CR is deleted. Both an app CR and configmap are created and the configmap is
+// deleted first.
+func TestSkipValidationOnDelete(t *testing.T) {
+	ctx := context.Background()
+
+	var err error
+
+	cm := &corev1.ConfigMap{
+		Data: map[string]string{
+			"values": "values",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "dex-config",
+			Namespace: "giantswarm",
+		},
+	}
+
+	app := &v1alpha1.App{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "dex-app-unique",
+			Namespace: "giantswarm",
+			Labels: map[string]string{
+				label.AppOperatorVersion: "0.0.0",
+			},
+		},
+		Spec: v1alpha1.AppSpec{
+			Catalog: "control-plane-catalog",
+			Config: v1alpha1.AppSpecConfig{
+				ConfigMap: v1alpha1.AppSpecConfigConfigMap{
+					Name:      "dex-config",
+					Namespace: "giantswarm",
+				},
+			},
+			Name:      "dex-app",
+			Namespace: "giantswarm",
+			KubeConfig: v1alpha1.AppSpecKubeConfig{
+				InCluster: true,
+			},
+			Version: "1.2.2",
+		},
+	}
+
+	logger.LogCtx(ctx, "level", "debug", "message", "creating app and configmap")
+
+	o := func() error {
+		_, err = appTest.K8sClient().CoreV1().ConfigMaps("giantswarm").Create(ctx, cm, metav1.CreateOptions{})
+		if apierrors.IsAlreadyExists(err) {
+			// fall through
+			return nil
+		} else if err != nil {
+			return microerror.Mask(err)
+		}
+
+		err = appTest.CtrlClient().Create(ctx, app)
+		if apierrors.IsAlreadyExists(err) {
+			// fall through
+			return nil
+		} else if err != nil {
+			return microerror.Mask(err)
+		}
+
+		return nil
+	}
+	b := backoff.NewConstant(5*time.Minute, 30*time.Second)
+	n := backoff.NewNotifier(logger, ctx)
+
+	err = backoff.RetryNotify(o, b, n)
+	if err != nil {
+		t.Fatalf("expected nil but got error %#v", err)
+	}
+
+	logger.LogCtx(ctx, "level", "debug", "message", "created app and configmap")
+
+	logger.LogCtx(ctx, "level", "debug", "message", "deleting configmap")
+
+	err = appTest.K8sClient().CoreV1().ConfigMaps("giantswarm").Delete(ctx, "dex-config", metav1.DeleteOptions{})
+	if err != nil {
+		t.Fatalf("expected nil but got error %#v", err)
+	}
+
+	logger.LogCtx(ctx, "level", "debug", "message", "deleted configmap")
+
+	logger.LogCtx(ctx, "level", "debug", "message", "deleting app")
+
+	err = appTest.CtrlClient().Delete(ctx, app)
+	if err != nil {
+		t.Fatalf("expected nil but got error %#v", err)
+	}
+
+	logger.LogCtx(ctx, "level", "debug", "message", "deleted app")
 }

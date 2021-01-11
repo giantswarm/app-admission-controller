6fa3ff1cc6b804fb7233070c5362961e27747d70
Paweł Kopiczko <pawel@giantswarm.io>
2021-01-11 14:39:32 +0000

    Update Makefile.gen.go.mk

--- Makefile.gen.go.mk
+++ Makefile.gen.go.mk
@@ -82,5 +82,4 @@ test:
 ## build-docker: builds docker image to registry
 build-docker: build-linux
 	@echo "====> $@"
-	cp $(APPLICATION)-linux $(APPLICATION)
 	docker build -t ${APPLICATION}:${VERSION} .

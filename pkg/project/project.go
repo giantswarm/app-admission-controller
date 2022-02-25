d27f871d3a2475522ee35e185441a8346debcdc9
GitHub Action <action@github.com>
2022-02-25 15:53:17 +0000

    Bump version to 0.16.4-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "app-admission-controller"
 	source      = "https://github.com/giantswarm/app-admission-controller"
-	version     = "0.16.3"
+	version     = "0.16.4-dev"
 )
 
 func Description() string {

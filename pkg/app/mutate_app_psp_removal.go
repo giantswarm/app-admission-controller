ccba4fbdae4179af7b0ef265690b4804026535b5
Stefan <64960813+Strigix@users.noreply.github.com>
2023-12-12 13:09:11 +0100

    Changes to AAC to check for and work with different providers (CAPx and Vintage) (#381)
    * first change to check for vintage cluster
    
    * added provider check and capi cluster label check
    
    * Update pkg/app/mutate_app_psp_removal.go
    
    Co-authored-by: Zach Stone <z.stone91@gmail.com>
    
    * Fix CAPI case
    
    * Add some logs
    
    * Fix label check for CAPI
    
    * Solve endless reconcilation problem
    
    * Update changelog
    
    * first extra test commit
    
    * added test for capi cluster with disable label missing
    
    * added capi cluster missing cr missing log and changed test 14
    
    * added test 15
    
    * Align with main (#402)
    
    * Update golang.org/x/exp digest to f3f8817 (#398)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update module github.com/spf13/viper to v1.18.0 (#397)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update module github.com/nats-io/nats-server/v2 to v2.10.7 (#399)
    
    * Update module github.com/nats-io/nats-server/v2 to v2.10.7
    
    * Remove ineffective replace
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Marian Steinbach <marian@giantswarm.io>
    
    * Update module github.com/spf13/viper to v1.18.1 (#401)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update alpine Docker tag to v3.19.0 (#400)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Marian Steinbach <marian@giantswarm.io>
    
    * fixed providers
    
    ---------
    
    Co-authored-by: Erkan Erol <erkanerol92@gmail.com>
    Co-authored-by: Zach Stone <z.stone91@gmail.com>
    Co-authored-by: Erkan Erol <erkan@giantswarm.io>
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Marian Steinbach <marian@giantswarm.io>

--- pkg/app/mutate_app_psp_removal.go
+++ pkg/app/mutate_app_psp_removal.go
@@ -3,19 +3,21 @@ package app
 import (
 	"context"
 	"fmt"
+	"reflect"
 	"strings"
 
 	"github.com/Masterminds/semver/v3"
-	"github.com/giantswarm/apiextensions-application/api/v1alpha1"
-	"github.com/giantswarm/app/v7/pkg/key"
-	"github.com/giantswarm/k8smetadata/pkg/label"
-	"github.com/giantswarm/microerror"
 	"golang.org/x/exp/slices"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	capiv1beta1 "sigs.k8s.io/cluster-api/api/v1beta1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/giantswarm/apiextensions-application/api/v1alpha1"
+	"github.com/giantswarm/app/v7/pkg/key"
+	"github.com/giantswarm/k8smetadata/pkg/label"
+	"github.com/giantswarm/microerror"
 
 	"github.com/giantswarm/app-admission-controller/config"
 	"github.com/giantswarm/app-admission-controller/pkg/mutator"
@@ -29,6 +31,7 @@ var (
 	// vintageProviders is a slice of provider names, like "aws";
 	// mutateConfigForPSPRemoval is applied to vintage providers exclusively
 	vintageProviders = []string{"aws", "azure", "kvm"}
+	capiProviders    = []string{"capa", "capz", "cloud-director", "vsphere"}
 )
 
 const (
@@ -39,30 +42,38 @@ const (
 	topPriority = 150
 	// pspLabel values have to match the ones defined in pss-operator.
 	// See https://github.com/giantswarm/pss-operator/blob/main/service/controller/handler/pssversion/create.go#L25
-	// pspLabelKey has been escaped ('/' replaced with '~1') to fit JSONPatch format.
-	pspLabelKey = "policy.giantswarm.io~1psp-status"
-	pspLabelVal = "disabled"
+	// pspLabelKeyForPatch has been escaped ('/' replaced with '~1') to fit JSONPatch format.
+	pspLabelKeyForPatch = "policy.giantswarm.io~1psp-status"
+	pspLabelKey         = "policy.giantswarm.io/psp-status"
+	pspLabelVal         = "disabled"
 )
 
 // mutateConfigForPSPRemoval is a temporary solution to
 // https://github.com/giantswarm/roadmap/issues/2716. Revert once migration to
 // Release >= v19.3.0 is complete and managed apps no longer rely on PSPs.
 func (m *Mutator) mutateConfigForPSPRemoval(ctx context.Context, app v1alpha1.App) ([]mutator.PatchOperation, error) {
-	result := []mutator.PatchOperation{}
+	m.logger.Debugf(ctx, "App mutation for PSP Removal. App:%s, Namespace:%s\n",
+		app.Name, app.Namespace)
 
-	if !slices.Contains(vintageProviders, strings.ToLower(m.provider)) {
-		// PSP patch is applicable to vintage providers only.
-		return result, nil
+	isVintageCluster := slices.Contains(vintageProviders, strings.ToLower(m.provider))
+	isCAPICluster := slices.Contains(capiProviders, strings.ToLower(m.provider))
+
+	if !isVintageCluster && !isCAPICluster {
+		return nil, microerror.Maskf(pspRemovalError, "unsupported provider for PSP deprecation: %s", m.provider)
 	}
+
+	result := []mutator.PatchOperation{}
 
 	clusterID := key.ClusterLabel(app)
 	if clusterID == "" {
 		// This App CR does not belong to any Workload Cluster - it does not
 		// need any more patches.
+		m.logger.Debugf(ctx, "App CR does not belong to any Workload Cluster. Skipping.\n")
 		return result, nil
 	}
 
 	if app.Labels[label.AppOperatorVersion] == "0.0.0" && app.Namespace == "giantswarm" {
+		m.logger.Debugf(ctx, "App is not WC app. Skipping.\n")
 		// This App is not a Workload Cluster app, but has a ClusterID
 		// annotation - it's an app bundle to be deployed to the MC.
 		return result, nil
@@ -100,61 +111,84 @@ func (m *Mutator) mutateConfigForPSPRemoval(ctx context.Context, app v1alpha1.Ap
 	if len(ec) > 0 && ec[len(ec)-1] == extraConfig {
 		// Ensure pssLabel to prevent any conflicts between pss-operator and other
 		// operators, like Flux.
-		result = append(result, mutator.PatchAdd(fmt.Sprintf("/metadata/labels/%s", pspLabelKey), pspLabelVal))
+		result = append(result, mutator.PatchAdd(fmt.Sprintf("/metadata/labels/%s", pspLabelKeyForPatch), pspLabelVal))
 
 		if err := m.ensureConfigMap(ctx, app.Namespace, extraConfigName, extraConfigValues); err != nil {
 			return nil, microerror.Mask(err)
 		}
+		m.logger.Debugf(ctx, "Extra config is already set. Skipping.\n")
 		return result, nil
 	}
 
 	// This App belongs to a Workload Cluster, which is using a certain Release
 	// version. Let's determine what it is.
-	var releaseVersion *semver.Version
-	{
-		// We don't want to guess Cluster's namespace because it's been
-		// historically difficult. Cluster ID/name is unique, so we are relying
-		// on that.
-		clusterCRList := capiv1beta1.ClusterList{}
-		err := m.k8sClient.CtrlClient().List(ctx, &clusterCRList, &client.ListOptions{})
-		if err != nil {
-			return nil, microerror.Maskf(pspRemovalError, "error listing Clusters: %v", err)
-		}
 
-		var clusterCR *capiv1beta1.Cluster
-		for _, item := range clusterCRList.Items {
-			if item.Name == clusterID {
-				x := item
-				clusterCR = &x
-				break
-			}
-		}
+	// We don't want to guess Cluster's namespace because it's been
+	// historically difficult. Cluster ID/name is unique, so we are relying
+	// on that.
+	clusterCRList := capiv1beta1.ClusterList{}
+	err := m.k8sClient.CtrlClient().List(ctx, &clusterCRList, &client.ListOptions{})
+	if err != nil {
+		return nil, microerror.Maskf(pspRemovalError, "error listing Clusters: %v", err)
+	}
 
-		if clusterCR == nil {
+	var clusterCR *capiv1beta1.Cluster
+	for _, item := range clusterCRList.Items {
+		if item.Name == clusterID {
+			x := item
+			clusterCR = &x
+			break
+		}
+	}
+
+	if clusterCR == nil {
+		if isVintageCluster {
 			return nil, microerror.Maskf(pspRemovalError, "could not find a Cluster CR matching %q among %d CRs", clusterID, len(clusterCRList.Items))
+		} else {
+			// In CAPI clusters, Cluster CR can be created after the App CR.
+			// pss-operator is responsible to trigger mutation of the App
+			// once Cluster CR exists and has the psp label.
+			m.logger.Debugf(ctx, "Could not find a Cluster CR, skipping and trust PSS-Operator.")
+			return result, nil
+		}
+	}
+
+	if isVintageCluster {
+		m.logger.Debugf(ctx, "Vintage provider %s detected, checking release version\n", m.provider)
+		var releaseVersion *semver.Version
+		{
+			label, ok := clusterCR.Labels[label.ReleaseVersion]
+			if !ok {
+				return nil, microerror.Maskf(pspRemovalError, "error inferring Release version for Cluster %q", clusterID)
+			}
+
+			releaseSemver, err := semver.NewVersion(label)
+			if err != nil {
+				return nil, microerror.Maskf(pspRemovalError, "error parsing Release version %q as semver: %v", label, err)
+			}
+
+			releaseVersion = releaseSemver
 		}
 
-		label, ok := clusterCR.Labels[label.ReleaseVersion]
+		if releaseVersion.LessThan(pssCutoffVersion) {
+			// releaseVersion is lower than pssCutoffVersion and still supports PSPs. Nothing to do.
+			return result, nil
+		}
+	} else if isCAPICluster {
+		m.logger.Debugf(ctx, "CAPI provider %s detected, checking cluster labels\n", m.provider)
+		disableLabel, ok := clusterCR.Labels[pspLabelKey]
+		// If the cluster CR does not have a label, we assume it still supports PSPs.
 		if !ok {
-			return nil, microerror.Maskf(pspRemovalError, "error infering Release version for Cluster %q", clusterID)
+			m.logger.Debugf(ctx, "Cluster doesn't have psp label. Skipping\n")
+			return result, nil
 		}
-
-		releaseSemver, err := semver.NewVersion(label)
-		if err != nil {
-			return nil, microerror.Maskf(pspRemovalError, "error parsing Release version %q as semver: %v", label, err)
+		if ok && disableLabel != pspLabelVal {
+			return nil, microerror.Maskf(pspRemovalError, "cluster %q label found, but not set to %q", pspLabelKey, pspLabelVal)
 		}
-
-		releaseVersion = releaseSemver
 	}
-
-	if releaseVersion.LessThan(pssCutoffVersion) {
-		// releaseVersion is lower than pssCutoffVersion and still supports PSPs. Nothing to do.
-		return result, nil
-	}
-
 	// Ensure pssLabel to prevent any conflicts between pss-operator and other
 	// operators, like Flux.
-	result = append(result, mutator.PatchAdd(fmt.Sprintf("/metadata/labels/%s", pspLabelKey), pspLabelVal))
+	result = append(result, mutator.PatchAdd(fmt.Sprintf("/metadata/labels/%s", pspLabelKeyForPatch), pspLabelVal))
 
 	// We need to ensure configMap disabling PSPs exists and is added to
 	// .spec.extraConfigs with highest priority.
@@ -169,6 +203,7 @@ func (m *Mutator) mutateConfigForPSPRemoval(ctx context.Context, app v1alpha1.Ap
 	}
 	result = append(result, mutator.PatchAdd("/spec/extraConfigs/-", extraConfig))
 
+	m.logger.Debugf(ctx, "Mutation for PSPs are done.\n", m.provider)
 	return result, nil
 }
 
@@ -188,16 +223,20 @@ func (m *Mutator) ensureConfigMap(ctx context.Context, namespace, name, values s
 		},
 	}
 
-	_, err := m.k8sClient.K8sClient().CoreV1().ConfigMaps(namespace).Create(ctx, cm, metav1.CreateOptions{})
-	if apierrors.IsAlreadyExists(err) {
-		_, err = m.k8sClient.K8sClient().CoreV1().ConfigMaps(namespace).Update(ctx, cm, metav1.UpdateOptions{})
-		if err != nil {
-			return microerror.Mask(err)
+	existing, err := m.k8sClient.K8sClient().CoreV1().ConfigMaps(namespace).Get(ctx, name, metav1.GetOptions{})
+	if err == nil {
+		if reflect.DeepEqual(existing.Data, cm.Data) {
+			m.logger.Debugf(ctx, "Configmap '%s' in '%s' namespace is up-to-date\n", name, namespace)
+			return nil
 		}
-	} else if err != nil {
-		return microerror.Mask(err)
+		m.logger.Debugf(ctx, "Updating configmap '%s' in '%s' namespace\n", name, namespace)
+		_, err = m.k8sClient.K8sClient().CoreV1().ConfigMaps(namespace).Update(ctx, cm, metav1.UpdateOptions{})
+	} else if apierrors.IsNotFound(err) {
+		m.logger.Debugf(ctx, "Creating configmap '%s' in '%s' namespace\n", name, namespace)
+		_, err = m.k8sClient.K8sClient().CoreV1().ConfigMaps(namespace).Create(ctx, cm, metav1.CreateOptions{})
 	}
-	return nil
+
+	return err
 }
 
 // appRequiresCustomPatch checks if a particular app has a defined, customized

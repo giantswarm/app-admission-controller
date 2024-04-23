33ff88a0f4fd5d36597c43e3e21d3e4f99de14b8
taylorbot <dev@giantswarm.io>
2024-04-23 11:00:39 +0000

    Bump version to 0.24.4-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "app-admission-controller" //nolint:gosec
 	source      = "https://github.com/giantswarm/app-admission-controller"
-	version     = "0.24.3"
+	version     = "0.24.4-dev"
 )
 
 func Description() string {

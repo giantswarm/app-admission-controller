42dca8d2f0151e117ca8550fdfe573456ce7a394
taylorbot <dev@giantswarm.io>
2024-11-19 09:08:03 +0000

    Release v1.0.0

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "app-admission-controller" //nolint:gosec
 	source      = "https://github.com/giantswarm/app-admission-controller"
-	version     = "0.26.3-dev"
+	version     = "1.0.0"
 )
 
 func Description() string {

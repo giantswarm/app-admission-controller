b4683f812098b9736239c7e4c63dd4d13c9948a6
Laszlo Uveges <laszlo@giantswarm.io>
2024-11-19 10:00:05 +0100

    Migrate to ABS, remove PSP support and bump architect-orb (#557)

--- integration/templates/value.go
+++ integration/templates/value.go
@@ -3,6 +3,13 @@
 
 package templates
 
+// CertManagerValues values required by cert-manager-app chart.
+const CertManagerValues = `
+global:
+  podSecurityStandards:
+    enforced: true
+`
+
 // AppAdmissionControllerValues values required by app-admission-controller chart.
 const AppAdmissionControllerValues = `
 provider:

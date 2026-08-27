5fce76190bac57d887e1108da91052bad0ae1db5
Łukasz Jakimczuk <lukasz.j@giantswarm.io>
2026-08-27 12:58:21 +0200

    After-review changes

--- integration/env/common.go
+++ integration/env/common.go
@@ -26,9 +26,13 @@ var (
 
 func init() {
 	filePath := filepath.Join(os.Getenv("CIRCLE_WORKING_DIRECTORY"), ".build_version")
-	buf, _ := os.ReadFile(filePath)
+	buf, err := os.ReadFile(filePath)
+	if err != nil {
+		panic(fmt.Sprintf("error reading .build_version: %v", err))
+	}
+
 	if string(buf) == "" {
-		panic(fmt.Sprintf(".build_version must not be empty"))
+		panic(".build_version must not be empty")
 	}
 	buildVersion = string(buf)
 
